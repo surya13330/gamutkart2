63eaf87d3f941b58786354c1c96009a99c86e973
surya13330 <surya13330@gmail.com>
2023-09-26 12:09:53 +0530

    adding some data

--- create-env.sh
+++ create-env.sh
@@ -5,10 +5,10 @@ sleep 2;
 for i in `seq $cont_count`
 do
 	echo "=============================="
-    echo "Creating tomcat-server$i container.."
+    echo "Creating gamutkart-server$i container.."
     sleep 1
     docker run -it -d --name tomcat-server$i nageshvkn/gamutkart-img
-    echo "tomcat-server$i container has been created!"
+    echo "gamutkart-server$i container has been created!"
 	echo "=============================="
 done
 docker inspect --format {{.NetworkSettings.Networks.bridge.IPAddress}} `docker ps -q` > IPs.txt
